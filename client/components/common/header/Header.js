cdb75ac8751409f8cde47da61f6bfe95a12b5dd8
harrell parker <harrellparker.d@gmail.com>
2019-02-25 02:24:35 -0500

    Update Header for admin user

--- client/components/common/header/Header.js
+++ client/components/common/header/Header.js
@@ -2,9 +2,16 @@ import React, {Component} from 'react';
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import {withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import * as authService from '../../../services/authService';
-import Grid from '@material-ui/core/Grid';
 
 const drawerWidth = 250;
 
@@ -33,40 +40,34 @@ const styles = theme => ({
     },
     flex: {
         flex: 1
-    },
-    colorBgItem: {
-        height: 20,
-        display: 'inline-block',
     }
 });
 
-const styleColors = [
-    "#e05333", 
-    "#ffc928",
-    "#fbe52f",
-    "#cde15e",
-    "#80cc33",
-    "#40bdcd"
-]
-
 class Header extends Component {
 
-    // logOut(e) {
-    //     e.preventDefault();
-    //     this.props.actions.logout();
-    // }
+    logOut(e) {
+        e.preventDefault();
+        this.props.actions.logout();
+    }
 
     render() {
-        const { classes } = this.props;
+        const {classes, navDrawerOpen, handleToggleDrawer} = this.props;
 
         return (
-            <header>
-                <Grid container spacing={24}>
-                    {styleColors.map(color =>
-                        <Grid item md={2} key={color} className={classes.colorBgItem} style={{backgroundColor: color }} />
-                    )}
-                </Grid>
-            </header>
+            <div>
+                <AppBar className={classNames(classes.appBar, navDrawerOpen && classes.appBarShift)}>
+                    <Toolbar>
+                        <IconButton aria-label="Menu" onClick={handleToggleDrawer}
+                                    className={classNames(!navDrawerOpen && classes.menuButton, navDrawerOpen && classes.menuButtonShift)}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography type="title" color="inherit" className={classes.flex}>
+
+                        </Typography>
+                        <Button onClick={this.logOut.bind(this)}>Logout</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
         )
     }
 }
