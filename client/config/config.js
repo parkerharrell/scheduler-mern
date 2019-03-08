af679a12f1e7fdc14a15a78ecfdd3e2ee15940a7
harrell parker <harrellparker.d@gmail.com>
2019-03-08 10:25:44 -0500

    Edit API Configs

--- client/config/config.js
+++ client/config/config.js
@@ -5,4 +5,5 @@ export const APP_PORT = process.env.APP_PORT;
 export const HOST = `http://${APP_HOST}:${APP_PORT}/`;
 
 export const BASE_URL = `${HOST}`;
-export const API_URL = `${process.env.API_URL}${apiPath}` || `${HOST}${apiPath}`;
+console.log('process.env.API_URL:', process.env.API_URL);
+export const API_URL = process.env.API_URL ? `${process.env.API_URL}${apiPath}` : `${HOST}${apiPath}`;
