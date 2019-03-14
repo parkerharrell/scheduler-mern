626351b784e4591e00dacb4c1dd9176aa74a05d5
Harrell Parker <harrellparker.d@gmail.com>
2019-03-13 23:34:52 -0500

    Update confirm page

--- client/components/confirm/index.js
+++ client/components/confirm/index.js
@@ -15,7 +15,21 @@ class ConfirmPage extends Component {
 	render() {
 		return (
 			<div>
-        Confirm Page
+				<br/>
+				<h1>Your Appointment Approved</h1>
+				<br/>
+				<p>Hi, this is Andrae Michaels Portrait Studio. This email is in regards to your Photo Sitting.</p> 
+
+				<p>Before your appointment, here’s a few reminders…</p>
+
+				<p>If you have invested in a promotional package from the mall: Please remember to bring in your certificate, and $10 sitting fee. The sitting fee is not per person, and covers up to 18 people. We recommend that you color coordinate your clothing and avoid bold logos or patterns. Pets are welcome in your session! When you come in we will take a variety of poses for you. You will be able to look at everything directly after, pick out all of your favorite shots, and which one pose you like for your promotional package. Be prepared to love all of them, and want more! We will go over all of our custom packages and pricing, and everything you order additionally will need to be purchased at that time. We accept credit, debit, and cash.</p> 
+
+				<p>If you are a Studio Member: <br/>
+				There is no sitting fee! Pets are welcome! We recommend that you color coordinate clothing and avoid bold logos or patterns. We will take a variety of poses for you, and upload them to your personal Flashop webpage.</p>
+
+				<p>If you have any questions, or need directions to our studio, don’t hesitate to give us a call! Thank you so much, and we look forward to seeing you and your family!</p>
+
+				<p>Thank you so much and we look forward to seeing you and your family.</p>
 			</div>
 		);
 	}
