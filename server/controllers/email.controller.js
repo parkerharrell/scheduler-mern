e51cc38412fd8dd6e595f06ecc90cd74f71a0b5f
Harrell Parker <harrellparker.d@gmail.com>
2019-04-03 10:03:08 -0500

    Add Emailing feature

--- server/controllers/email.controller.js
+++ server/controllers/email.controller.js
@@ -1,15 +1,49 @@
 import HttpStatus from 'http-status-codes';
 import User from '../models/user.model';
 import Admin from '../models/admin.model';
+const sgMail = require('@sendgrid/mail');
 
-/**
- * Find all the emails
- *
- * @param {object} req
- * @param {object} res
- * @returns {*}
- */
+function sendMail(to, subject, message, res) {
+  const msg = {
+    to: to,
+    from: 'projects@andraemichaels.com',
+    subject,
+    text: message,
+    html: message,
+  };
+  sgMail.send(msg, function(err, json) {
+    if (err) { 
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        error: err
+      });
+    }
+    return res.json({
+      error: false,
+    });
+  });
+}
+
 export function store(req, res) {
-  const { subject, message } = req.body;
-  
+  const { subject, message, to, toAdmin } = req.body;
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  if (to) {
+    sendMail(to, subject, message);
+  } else if (toAdmin) {
+    User
+      .forge()
+      .fetchAll()
+      .then(users => {
+        const emails = users.map(item => item.email);
+        sendMail(emails, subject, message, res);
+      })
+  } else {
+    Admin
+      .forge()
+      .fetchAll()
+      .then(admins => {
+        const emails = admins.map(item => item.email);
+        sendMail(emails, subject, message, res);
+      })
+  }
 }
--- server/routes/email.route.js
+++ server/routes/email.route.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import * as emailCtrl from '../controllers/email.controller';
-import isAuthenticated from '../middlewares/authenticate';
-import validate from '../config/joi.validate';
-import schema from '../utils/validator';
 
 const router = express.Router();
 
